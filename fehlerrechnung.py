bc7d440e3b691b1430001ae26280adcd6a39472b
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2023-05-07 12:44:37 +0200

    AUswertung lala

--- fehlerrechnung.py
+++ fehlerrechnung.py
@@ -16,6 +16,14 @@ def radius2(eta,vab,vauf):
 def radius1(eta,v0):
     return np.sqrt((9*eta*v0)/(2*9.81*(886-1.204)))
 
+def neff(n,p,r):
+    return n*(1/(1+6.17*1**(-5)*1/p*r))
+
+def q(n,vab,vauf,V):
+    return 3*np.pi*n*np.sqrt(9*n*(vab-vauf)/2**9.81*(886-1.204))*(vab-vauf)/(V/d)
+
+p=101300
+du= ufloat(7.6250, 0.0051)
 eta205 = 1.844*10**(-5)
 eta208 = 1.840*10**(-5)
 eta217 = 1.834*10**(-5)
@@ -32,8 +40,11 @@ v2520  =0.0103
 
 print(radius2(eta217, vab, vauf))
 
-print('Radius 1 250V Tröpchen 14',radius1(eta205,v2510))
-print('Radius 1 250V Tröpchen 16',radius1(eta205,v2520))
+#print('Radius 1 250V Tröpfchen 14',radius1(eta205,v2510))
+#print('Radius 1 250V Tröpfchen 16',radius1(eta205,v2520))
 
-print('Radius 2 250V Tröpchen 14',radius2(eta205,v251ab,v251auf))
-print('Radius 2 250V Tröpchen 16',radius2(eta205,v252ab,v252auf))
+print('Radius 2 250V Tröpfchen 14',radius2(eta205,v251ab,v251auf))
+print('Radius 2 250V Tröpfchen 16',radius2(eta205,v252ab,v252auf))
+
+print('Korrigiert 14',neff(eta205,p,radius2(eta205,v251ab,v251auf)))
+print('Korrigiert 16',neff(eta205,p,radius2(eta205,v252ab,v252auf)))
