f44d23822c754a643bc94a2257d9b4b0af013167
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2023-05-07 12:16:53 +0200

    AUswertung lala

--- fehlerrechnung.py
+++ fehlerrechnung.py
@@ -10,8 +10,11 @@ from uncertainties.unumpy import uarray                     # Array von Fehler: 
 from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
                                   std_devs as stds)  
 
-def radius(eta,vab,vauf):
+def radius2(eta,vab,vauf):
     return np.sqrt((9*eta*(vab-vauf))/(2*9.81*(886-1.204)))
+
+def radius1(eta,v0):
+    return np.sqrt((9*eta*v0)/(2*9.81*(886-1.204)))
 
 eta205 = 1.844*10**(-5)
 eta208 = 1.840*10**(-5)
@@ -20,4 +23,17 @@ eta220 = 1.832*10**(-5)
 vab = 0.2053
 vauf = 0.01641
 
-print(radius(eta217, vab, vauf))
+v251auf=0.0587
+v251ab =0.1042
+v252auf=0.0466
+v252ab = 0.6027
+v2510  =0.0228
+v2520  =0.0103
+
+print(radius2(eta217, vab, vauf))
+
+print('Radius 1 250V Tröpchen 14',radius1(eta205,v2510))
+print('Radius 1 250V Tröpchen 16',radius1(eta205,v2520))
+
+print('Radius 2 250V Tröpchen 14',radius2(eta205,v251ab,v251auf))
+print('Radius 2 250V Tröpchen 16',radius2(eta205,v252ab,v252auf))
